6c848e1bfd11b21fef36be70505aa57e7a513b11
camila <camila@camila-Aspire-A315-42G>
2021-09-27 19:35:01 -0300

    livro de algoritmos - cap04

--- src/livroAlgoritmos/cap04/ex03a.kt
+++ src/livroAlgoritmos/cap04/ex03a.kt
@@ -1,4 +1,22 @@
 package livroAlgoritmos.cap04
 
+import java.util.*
+
 //Efetuar a leitura de dois valores numéricos inteiros representados pelas variáveis A e B
-//e apresentaro resultado da diferença do maior valor pelo menor valor
+//e apresentar o resultado da diferença do maior valor pelo menor valor
+
+fun main() {
+
+    val scan = Scanner(System.`in`).useLocale(Locale.US)
+    println("Digite um número:")
+    val a = scan.nextInt()
+
+    println("Digite um número:")
+    val b = scan.nextInt()
+
+    if (a > b) {
+        println(a - b)
+    } else {
+        println(b - a)
+    }
+}
