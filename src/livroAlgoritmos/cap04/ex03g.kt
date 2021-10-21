f1a4cf72c7851463e61c3a7dda3c614d7a93fe12
camila <camila@camila-Aspire-A315-42G>
2021-10-21 19:27:39 -0300

    livro de algoritmos - cap04

--- src/livroAlgoritmos/cap04/ex03g.kt
+++ src/livroAlgoritmos/cap04/ex03g.kt
@@ -1,4 +1,46 @@
 package livroAlgoritmos.cap04
 
+import java.util.*
+
 //Fazer a leitura de quatro valores numéricos inteiros representados pelas variáveis A,B, C e D.
 //Apresentar apenas os valores que sejam divisiveis por 2 e 3.
+
+fun main() {
+
+    val scan = Scanner(System.`in`).useLocale(Locale.US)
+    println("Digite um valor para a variável a:")
+    val a = scan.nextInt()
+
+    println("Digite um valor para a variável b:")
+    val b = scan.nextInt()
+
+    println("Digite um valor para a variável c:")
+    val c = scan.nextInt()
+
+    println("Digite um valor para a variável d:")
+    val d = scan.nextInt()
+
+    if (a % 2 == 0 && a % 3 == 0) {
+        println("$a é divisível por 2 e 3")
+    } else {
+        println("O valor apresentado não é divisível por 2 e 3")
+    }
+
+    if (b % 2 == 0 && b % 3 == 0) {
+        println("$a é divisível por 2 e 3")
+    } else {
+        println("O valor apresentado não é divisível por 2 e 3")
+    }
+
+    if (c % 2 == 0 && c % 3 == 0) {
+        println("$a é divisível por 2 e 3")
+    } else {
+        println("O valor apresentado não é divisível por 2 e 3")
+    }
+
+    if (d % 2 == 0 && d % 3 == 0) {
+        println("$a é divisível por 2 e 3")
+    } else {
+        println("O valor apresentado não é divisível por 2 e 3")
+    }
+}
