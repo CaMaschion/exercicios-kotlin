4bc7ed98b2a9b3cf39db3f8efe5f4822cce440dd
camila <cm.maschion@gmail.com>
2022-04-14 19:42:04 +0100

    hacker rank

--- src/hackerRank/birthdayCakeCandle.kt
+++ src/hackerRank/birthdayCakeCandle.kt
@@ -2,39 +2,48 @@ package hackerRank
 
 fun birthdayCakeCandles(candles: Array<Int>): Int {
 
-    var highestCandle = 1
+    var tallest = 0
     var counter = 0
 
     //descobrir qual a vela mais alta no array
-    for(i in candles.indices) {
-        if(candles[i] > highestCandle) {
-            highestCandle = candles[i]
+    for(candle in candles.indices) {
+        if(candles[candle] > tallest) {
+            tallest = candle
         }
     }
 
     // quantas vezes a vela mais alta aparece
-    for(i in candles.indices) {
-        if(candles[i] == highestCandle){
+    for(candle in candles.indices) {
+        if(candles[candle] == tallest){
             counter++
         }
     }
     return counter
 }
 
+fun birthdayCakeCandles1(candles: Array<Int>): Int {
+    val tallest = candles.maxOrNull()
+    return candles.count{ it == tallest}
+}
+
 fun birthdayCakeCandles2(candles: Array<Int>): Int {
-    val highestCandle = candles.maxOrNull()
+    val tallest = candles.maxOrNull()
     var counter = 0
 
     for(i in candles.indices) {
-        if(candles[i] == highestCandle){
+        if(candles[i] == tallest){
             counter++
         }
     }
     return counter
+}
+
+fun birthdayCakeCandles3(candles: Array<Int>): Int {
+    return candles.count{ it == candles.maxOrNull()}
 }
 
 fun main(args: Array<String>) {
 
-    println(birthdayCakeCandles2(arrayOf(3,2,1,3)))
+    println(birthdayCakeCandles3(arrayOf(3,2,1,3)))
 
 }
