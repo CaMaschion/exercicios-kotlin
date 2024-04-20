4a23e3da41654697b79cd8553111c471f3462e35
Camila Maschion <cm.maschion@gmail.com>
2024-04-20 17:08:32 +0100

    exercicio cs50x semana 1 + livro

--- src/exemplos/customPasswordKeyboard.kt
+++ src/exemplos/customPasswordKeyboard.kt
@@ -6,18 +6,9 @@ fun main(){
 
 fun checkIfButtonPressSequenceIsValid(buttonPressSequence: Array<Int>): Boolean {
 
-    val keyboard = arrayOf(Pair(0,1), Pair(2,3), Pair(3,4), Pair(5,6), Pair(7,8), Pair(9,0))
+    val keyboard = arrayOf(Pair(0,1), Pair(2,3), Pair(4,5), Pair(6,7), Pair(8,9))
 
     val password = "96482"
-
-   //buttonPressSequence -> Array é a sequencia que representa meu password(senha)
-   //keyboard -> representacao do teclado
-   //password -> minha senha
-
-    //para encontrar o primeiro número da senha digitado:
-    //clico no keyboard, numero 9/indice 5 do keyboard (primeiro numero da password)
-    //percorrer a sequencia (buttonPressSequence) para validar se a password está correta
-    //validar se a posicao(buttonIndex) do keyboard e a posicao da password são iguais
 
     for (buttonIndex in buttonPressSequence){
         if (keyboard[buttonIndex].first.toString() == password[0].toString() ||
