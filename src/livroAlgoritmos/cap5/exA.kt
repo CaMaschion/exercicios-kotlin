fb9abcb737cbbc91cc4a0f868d52d2076fd56235
Camila Maschion <cm.maschion@gmail.com>
2024-05-24 18:53:36 +0100

    when

--- src/livroAlgoritmos/cap5/exA.kt
+++ src/livroAlgoritmos/cap5/exA.kt
@@ -5,9 +5,12 @@ package livroAlgoritmos.cap5
 
 /**  Passo a Passo
  *
- * 1 Definir intervalos de numeros
- * 2 calcular o quadrado dos numeros
- * 3 armazenar o resultado
+ * 1 Iniciar o Contador: Configure um contador para iniciar em 15 e terminar em 200.
+ * Este contador vai passar por cada número nesse intervalo.
+ * 2 Calcular o Quadrado: Para cada número nesse intervalo, calcule o quadrado do número.
+ * 3 armazenar o resultado: Armazene cada resultado (quadrado do número) em uma estrutura de dados.
+ * Uma lista seria adequada para isso se você estiver usando uma linguagem de programação que suporte listas
+ * (como Python, Java, C#).
  * 4 iteracao sobre o intervalo de numeros
  * 5 apresentacao dos resultados
  *
